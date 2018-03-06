4c72996feafa1fda0345ecf6bacee5bd161652bf
Eric Thai <233ericthai@gmail.com>
2018-03-06 15:17:24 -0500

    intialized map

---
+++ SafeArriavls/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.map}
+          region={{
+            latitude:59.32,
+            longitude: 18.06,
+            latitudeDelta:0.1,
+            longitudeDelta:0.1
+          }}
+        >
+        </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0
+  }
+});
