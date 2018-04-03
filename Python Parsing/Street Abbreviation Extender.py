d57cd2855342ff7df0d09aad9d0ebbb0256c9ba1
Victor <24923454+VictorPlusC@users.noreply.github.com>
2018-04-02 22:21:48 -0400

    Works with .csv instead now

--- Python Parsing/Street Abbreviation Extender.py
+++ Python Parsing/Street Abbreviation Extender.py
@@ -4,23 +4,33 @@ Converts:
     AV = AVE
     WY = WAY
 """
+import csv
 
-def script(filename: ".txt with no extension"):
-    with open(filename+".txt", "r") as f, open(filename+"New.txt", "w") as f2:
-        contents = f.readlines()
+def script(filename: ".csv with no extension"):
+    with open(filename+".csv", newline = '') as f, open(filename+"New.txt", "w") as f2:
+        contents = csv.reader(f)
         for row in contents:
-            splt = row.split()
-            tmp = []
-            for i in range(len(splt)):
-                if splt[i] == "BL":
-                    tmp.append("BLVD ")
-                elif splt[i] == "AV":
-                    tmp.append("AVE ")
-                elif splt[i] == "WY":
-                    tmp.append("WAY ")
-                else:
-                    tmp.append(splt[i] +" ")
-            f2.write("".join(tmp)+"\n")
-            print("".join(tmp)+"\n")
+            formatted = (row[0].split("\t"))
 
-script("parsedCollisions")
+            st1 = extendSt(formatted[3])
+            st2 = extendSt(formatted[4])
+            
+            toWrite = formatted[0]+","+formatted[1]+","+formatted[2]+","+st1+","+ st2 +"\n"
+            print(toWrite)
+            f2.write(toWrite)
+            
+def extendSt(street):
+    tmp = []
+    splt = street.split()
+    for i in range(len(splt)):
+        if splt[i] == "BL":
+            tmp.append("BLVD ")
+        elif splt[i] == "AV":
+            tmp.append("AVE ")
+        elif splt[i] == "WY":
+            tmp.append("WAY ")
+        else:
+            tmp.append(splt[i] +" ")
+    return "".join(tmp[:len(tmp)])[:len("".join(tmp[:len(tmp)]))-1]
+
+script("LA_DATA")
